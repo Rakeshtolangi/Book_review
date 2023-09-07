2cb3b73b86f9a8113fd5bbeffe9689dbf042a95f
Rakesh Tolangi <tolangirakesh67@gmail.com>
2023-09-07 07:05:18 +0545

    refactor: Get, Post and query dict
    by passing parameter values in the browser using  .....8000?name=Rakesh
    shows Hello rakesh.

--- bookr/reviews/views.py
+++ bookr/reviews/views.py
@@ -2,4 +2,5 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world!")
+    name = request.GET.get("name") or "world"
+    return HttpResponse("Hello, {}!".format(name))
